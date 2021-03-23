2e0d4f6dc349fd76999057a1597d95b10fe930cd
Samuel Colvin <s@muelcolvin.com>
2021-03-23 11:26:58 +0000

    simple tests

---
+++ cli/tests/conftest.py
@@ -0,0 +1,62 @@
+import asyncio
+from datetime import datetime, timedelta
+
+import pytest
+from aiohttp import web
+from aiohttp.abc import Request
+from aiohttp.web_response import json_response
+from foxglove.test_server import create_dummy_server
+
+
+async def create(request: Request):
+    now = datetime.now()
+    host = request.headers['host']
+    request.app['sites'].append(request.headers['authorisation'])
+    return json_response(
+        {
+            'message': 'New site created successfully',
+            'secret_key': 'testing',
+            'site_creation': now.isoformat(),
+            'site_expiration': (now + timedelta(days=30)).isoformat(),
+            'sites_created_24h': 0,
+            'upload_expiration': (now + timedelta(hours=1)).isoformat(),
+            'url': f'http://{host}/testing-site/',
+        }
+    )
+
+
+async def upload(request: Request):
+    body = await request.read()
+    request.app['files'][request.path] = body
+    return json_response({'size': 123, 'total_site_size': 1234})
+
+
+routes = [
+    web.post('/create/', create),
+    web.post('/testing-site/{file:.*}', upload),
+]
+
+
+@pytest.fixture(name='loop')
+def fix_loop():
+    try:
+        loop = asyncio.get_event_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    return loop
+
+
+@pytest.fixture(name='await_')
+def fix_await(loop):
+    return loop.run_until_complete
+
+
+@pytest.fixture(name='dummy_server')
+def _fix_dummy_server(loop):
+    ctx = {'sites': [], 'files': {}}
+    ds = loop.run_until_complete(create_dummy_server(loop, extra_routes=routes, extra_context=ctx))
+
+    yield ds
+
+    loop.run_until_complete(ds.stop())
---
+++ cli/tests/test_upload.py
@@ -0,0 +1,15 @@
+import re
+
+from foxglove.test_server import DummyServer
+
+from smokeshow import upload
+
+
+def test_upload_file(tmp_path, dummy_server: DummyServer, await_):
+    f = tmp_path / 'test.html'
+    f.write_text('<h1>testing</h1>')
+    url = await_(upload(f, 'testing-auth-key', root_url=dummy_server.server_name))
+    assert re.fullmatch(r'http://localhost:\d+/testing-site/', url)
+
+    assert dummy_server.app['sites'] == ['testing-auth-key']
+    assert dummy_server.app['files'] == {'/testing-site/test.html': b'<h1>testing</h1>'}
