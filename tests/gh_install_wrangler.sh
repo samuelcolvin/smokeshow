b0eee29650ec6e9aec34415388bca2b59b4f350a
Samuel Colvin <s@muelcolvin.com>
2022-07-15 11:25:14 +0100

    try to fix build

--- tests/gh_install_wrangler.sh
+++ tests/gh_install_wrangler.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -L https://github.com/cloudflare/wrangler/releases/download/v1.15.0/wrangler-v1.15.0-x86_64-unknown-linux-musl.tar.gz > wrangler.tar.gz
+curl -L https://github.com/cloudflare/wrangler/releases/download/v1.19.12/wrangler-v1.19.12-x86_64-unknown-linux-musl.tar.gz > wrangler.tar.gz
 tar -xvzf wrangler.tar.gz
 mkdir -p $HOME/bin
 mv dist/wrangler $HOME/bin
